c33a8102841430c4ef366788fcc069e843f0251b
sharath-m <sharath.rajeeva@gmail.com>
2013-09-16 16:40:44 +0100

    Added TodoApi

--- src/main/java/uk/co/malavalli/api/PingApi.java
+++ src/main/java/uk/co/malavalli/api/PingApi.java
@@ -4,6 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 
@@ -11,6 +12,8 @@ import org.apache.cxf.jaxrs.model.wadl.Description;
 import org.apache.cxf.jaxrs.model.wadl.Descriptions;
 import org.apache.cxf.jaxrs.model.wadl.DocTarget;
 import org.joda.time.DateTime;
+
+import uk.co.malavalli.api.common.MimeTypes;
 
 @Path("/ping")
 public class PingApi {
@@ -20,9 +23,19 @@ public class PingApi {
 			@Description(value = "Pings the API, interacting with no external services. Returns the current version of the application. " +
 					" Example: https://localhost:8080/SimpleWeb/ping", target = DocTarget.METHOD)
 	})
-	@Produces({ "text/plain" })
+	@Produces({ MimeTypes.PLAIN_TEXT })
 	public Response get(@Context Request request) {
 		String responseBody = String.format("Ping OK - %s", DateTime.now());
+		return Response.ok(responseBody).build();
+	}
+
+	@GET
+	@Descriptions({
+			@Description(value = "", target = DocTarget.METHOD)
+	})
+	@Produces({ MediaType.TEXT_XML })
+	public Response getAsXml(@Context Request request) {
+		String responseBody = "<?xml version=\"1.0\"?>" + "<ping> OK - " + DateTime.now() + "</ping>";
 		return Response.ok(responseBody).build();
 	}
 }
--- src/test/java/uk/co/malavalli/api/PingTest.java
+++ src/test/java/uk/co/malavalli/api/PingTest.java
@@ -1,44 +1,31 @@
 package uk.co.malavalli.api;
 
 import static com.jayway.restassured.RestAssured.*;
+//import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.jayway.restassured.RestAssured;
-
-import uk.co.malavalli.server.IntegrationTestServer;
-
 @ContextConfiguration(locations = { "classpath:application-context.xml" })
 @RunWith(SpringJUnit4ClassRunner.class)
-public class PingTest {
+public class PingTest extends AbstractApiTest {
 
-	Logger LOG = LoggerFactory.getLogger(getClass());
+	protected Logger LOG = LoggerFactory.getLogger(getClass());
 
-	@Autowired
-	IntegrationTestServer integrationTestServer;
-
-	@Before
-	public void before() throws Exception {
-		RestAssured.port = 8989;
-		integrationTestServer.start();
-	}
-
-	@After
-	public void after() throws Exception {
-		integrationTestServer.stop();
+	@Test
+	public void ping() throws Exception {
+		String asString = get("/ping").asString();
+		LOG.info(asString);
 	}
 
 	@Test
-	public void testname() throws Exception {
-		String asString = get("/ping").asString();
-		LOG.info(asString);
+	public void pingXml() throws Exception {
+		LOG.info(given().expect().contentType("text/xml").get("/ping").asString());
+		given().expect().statusCode(200).contentType("text/xml").body(containsString("<ping>")).get("/ping");
 	}
 }
